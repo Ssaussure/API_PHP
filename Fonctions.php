3c30c817f5d878c83a6fdaee19751d3fabc8c3eb
LineCabaret <102655775+LineCabaret@users.noreply.github.com>
2023-03-10 09:13:22 +0100

    Add files via upload

--- Fonctions.php
+++ Fonctions.php
@@ -1,11 +1,48 @@
 <?php
 	
-	function isValidUser(){
+	$users = array(
+		array(
+			"username" => "CbrtLine",
+			"password" => "iutinfo",
+			"role" => ""
+		),
+		array(
+			"username" => "AlrdFabien",
+			"password" => "FabInfo",
+			"role" => ""
+		)
+	)
+
+	function isValidUser($username, $password){
+
+		global $users;
+
+		foreach ($users as $user) {
+		    if ($user["username"] == $username) {
+		    	if ($user["password"] == $password){
+		    		return TRUE;
+		    	}
+		    	return False;
+		    }
+		    return False;
+		}
 
 	}
 
-	function get_user_role(){
+	/// Permet de récuperer le rôle d'un utilisateur
+	/// Cette fonction n'est appelée qu'une fois que l'identification est vérifiée
+	function get_user_role($username) {
 
+		global $users;
+
+		foreach ($users as $user) {
+		    if ($user["username"] == $username) {
+		    	return $user["role"];
+		    }
+		}
+
+		// Si aucun utilisateur n'est trouvé avec le nom d'utilisateur spécifié, retourner null
+		return null;
 	}
-	
+
 ?>
