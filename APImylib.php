eee93f81c1babf6167abe2369080e28fa5acc630
Allard Fabien <119335511+Ssaussure@users.noreply.github.com>
2023-03-29 16:59:35 +0200

    Add files via upload

--- APImylib.php
+++ APImylib.php
@@ -1,148 +1,210 @@
 <?php
-function connexionBd()
-{
-    // informations de connection
-    $SERVER = '127.0.0.1';
-    $DB = 'projetphp';
-    $LOGIN = 'root';
-    $MDP = '';
-    // tentative de connexion à la BD
-    try {
-        // connexion à la BD
-        $linkpdo = new PDO("mysql:host=$SERVER;dbname=$DB", $LOGIN, $MDP);
-    } catch (Exception $e) {
-        die('Erreur ! Problème de connexion à la base de données : ' . $e->getMessage());
+    function connexionBd()
+    {
+        // informations de connection
+        $SERVER = '127.0.0.1';
+        $DB = 'projetphp';
+        $LOGIN = 'root';
+        $MDP = '';
+        // tentative de connexion à la BD
+        try {
+            // connexion à la BD
+            $linkpdo = new PDO("mysql:host=$SERVER;dbname=$DB", $LOGIN, $MDP);
+        } catch (Exception $e) {
+            return null;
+        }
+        // retourne la connection
+        return $linkpdo;
     }
-    // retourne la connection
-    return $linkpdo;
-}
 
-function getId($id)
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('select * from articles where id = :id');
-    if ($req == false) {
-        die('Erreur !');
-    }
-    // execution de la Requête sql
-    $req->execute(array('id' => $id));
-    if ($req == false) {
-        die('Erreur !');
-    }
-    return $req->fetchAll();
-}
+	
+	$users = array(
+		array(
+			"username" => "CbrtLine",
+			"password" => "iutinfo",
+			"role" => "Publisher"
+		),
+		array(
+			"username" => "AlrdFabien",
+			"password" => "FabInfo",
+			"role" => "Moderator"
+		)
+	);
 
-function getBySignalement()
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('select * from articles where signalement > 0');
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    // execution de la Requête sql
-    $req->execute();
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    return $req->fetchAll();
-}
+	function isValidUser($username, $password){
 
-function getByVote()
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('select * from articles where like_pub > 3 order by like_pub desc');
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    // execution de la Requête sql
-    $req->execute();
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    return $req->fetchAll();
-}
+		global $users;
 
-function getByLast10()
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('select * from articles order by id desc limit 10');
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    // execution de la Requête sql
-    $req->execute();
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    return $req->fetchAll();
-}
+		foreach ($users as $user) {
+		    if ($user["username"] == $username) {
+		    	if ($user["password"] == $password){
+		    		return True;
+		    	}
+		    	return False;
+		    }
+		}
+		return False;
+	}
 
-function getAll()
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('select * from articles');
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    // execution de la Requête sql
-    $req->execute();
-    if ($req == false) {
-        die('Erreur ! GetAll');
-    }
-    return $req->fetchAll();
-}
+	/// Permet de récuperer le rôle d'un utilisateur
+	/// Cette fonction n'est appelée qu'une fois que l'identification est vérifiée
+	function get_user_role($username) {
 
-function post($publication, $auteur)
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('insert into articles (publication,auteur,like_pub,dislike,date, signalement) value(:publication,:auteur, 0,0,NOW(),0)');
-    if ($req == false) {
-        die('Erreur ! Post');
-    }
-    // execution de la Requête sql
-    $req->execute(array(':publication' => $publication, ':auteur' => $auteur));
-    if ($req == false) {
-        die('Erreur ! Post');
-    }
-    // recuperation du dernier id
-    $lastId = $linkpdo->lastInsertId();
-    return getId($lastId);
-}
+		global $users;
 
-function put($id, $publication)
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('update articles set publication = :publication where id = :id');
-    if ($req == false) {
-        die('Erreur ! Put');
-    }
-    // execution de la Requête sql
-    $req->execute(array('id' => $id, ':publication' => $publication));
-    if ($req == false) {
-        die('Erreur ! Put');
-    }
-    // recuperation du dernier id
-    return getId($id);
-}
+		foreach ($users as $user) {
+		    if ($user["username"] == $username) {
+		    	return $user["role"];
+		    }
+		}
 
-function delete($id)
-{
-    $linkpdo = connexionBd();
-    // preparation de la Requête sql
-    $req = $linkpdo->prepare('delete from articles where id = :id');
-    if ($req == false) {
-        die('Erreur ! Delete');
+		// Si aucun utilisateur n'est trouvé avec le nom d'utilisateur spécifié, retourner null
+		return null;
+	}
+
+
+    function getId($linkpdo, $id)
+    {
+        if(empty($id) || !is_numeric($id)) {
+            return null;
+        }
+        // preparation de la Requête sql
+        try{
+            $req = $linkpdo->prepare('select * from articles where id = :id');
+        } catch(PDOException $e) {
+            return null;
+        }
+        
+        if($req->bindParam(':id', $id, PDO::PARAM_INT)) {
+            if($req->execute()) {
+                return $req->fetchAll(PDO::FETCH_ASSOC);
+            }
+        }
+        return null;
     }
-    // execution de la Requête sql
-    $req->execute(array('id' => $id));
-    if ($req == false) {
-        die('Erreur ! Delete');
+
+
+
+    function getAll($linkpdo)
+    {
+        try{
+            $req = $linkpdo->prepare('select * from articles');
+        }catch(PDOException $e){
+            return null;
+        }
+        
+        if ($req == false) {
+            die('Erreur ! GetAll');
+        }
+        if($req->execute()) {
+            return $req->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
-}
+
+    function post($publication, $linkpdo, $jwt) {
+        try{
+            $req = $linkpdo->prepare('insert into articles (publication,auteur,like_pub,dislike,date, signalement) value(:publication,:auteur, 0,0,NOW(),0)');
+        }catch(PDOException $e) {
+            return null;
+        }
+        if ($req -> bindParam(':publication', $publication, PDO::PARAM_STR, ':auteur',get_role_jwt($jwt), PDO::PARAM_STR)) {
+            if($req->execute()) {
+                return $req->fetchAll(PDO::FETCH_ASSOC);
+            }
+        $id = $linkpdo->lastInsertId();
+        return getId($id);
+    }
+        }
+
+
+    function put($id, $publication, $linkpdo)
+    {
+        if(empty($id) || !is_numeric($id)) {
+            return null;
+        }
+
+        try {
+            // preparation de la Requête sql
+            $req = $linkpdo->prepare('update articles set publication = :publication where id = :id');
+        } catch(PDOException $e) {
+            return null;
+        }
+        if ($req -> bindParam(':publication', $publication, PDO::PARAM_STR, ':id', $id, PDO::PARAM_INT)) {
+            if($req->execute()) {
+                return $req->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+        }
+    }
+
+    function delete($id, $linkpdo){
+        if(empty($id) || !is_numeric($id)) {
+            return null;
+        }
+        try {
+            $req = $linkpdo->prepare('delete from articles where id = :id');
+        } catch(PDOException $e) {
+            return null;
+        }
+        if($req -> bindParam(':id', $id, PDO::PARAM_INT)) {
+            if($req->execute()){
+                return $req->fetchAll(PDO::FETCH_ASSOC);
+            }
+        }
+    }
+
+    function get_role_jwt($jwt) {
+
+        // Vérification de validité
+        if (!is_jwt_valid($jwt)) {
+            // Jeton invalide ou expiré
+            return null;
+        }
+
+        $payload = base64_decode(explode('.', $jwt)[1]);
+        $decoded_payload = json_decode($payload, true);
+
+        if (!isset($decoded_payload['role'])) {
+            // Rôle non trouvé dans le jeton
+            return null;
+        }
+
+        return $decoded_payload['role'];
+    }
+
+    function get_usename_jwt($jwt) {
+
+        // Vérification de validité
+        if (!is_jwt_valid($jwt)) {
+            // Jeton invalide ou expiré
+            return null;
+        }
+
+        $payload = base64_decode(explode('.', $jwt)[1]);
+        $decoded_payload = json_decode($payload, true);
+
+        if (!isset($decoded_payload['username'])) {
+            // Rôle non trouvé dans le jeton
+            return null;
+        }
+
+        return $decoded_payload['username'];
+    }
+
+    function is_id_exist($id) {
+        // Connexion à la BD
+        $linkpdo = connexionBd();
+        
+        // Requête pour vérifier si l'id existe dans la table appropriée
+        $req = $linkpdo->prepare("SELECT id FROM articles WHERE id=:id");
+        $req->bindParam(':id', $id);
+        $req->execute();
+        
+        // Si l'id est trouvé, renvoie true, sinon false
+        if ($req->rowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+?>
