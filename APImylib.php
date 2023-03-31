6c90a713debff128e3dab2eebac3f678676ddf8f
LineCabaret <102655775+LineCabaret@users.noreply.github.com>
2023-03-31 21:35:01 +0200

    Add files via upload

--- APImylib.php
+++ APImylib.php
@@ -1,210 +1,167 @@
 <?php
-    function connexionBd()
-    {
-        // informations de connection
-        $SERVER = '127.0.0.1';
-        $DB = 'projetphp';
-        $LOGIN = 'root';
-        $MDP = '';
-        // tentative de connexion à la BD
-        try {
-            // connexion à la BD
-            $linkpdo = new PDO("mysql:host=$SERVER;dbname=$DB", $LOGIN, $MDP);
-        } catch (Exception $e) {
-            return null;
-        }
-        // retourne la connection
-        return $linkpdo;
+
+include('Erreurs.php');
+
+function connexionBd(){
+
+    // informations de connection
+    $SERVER = '127.0.0.1';
+    $DB = 'projetphp';
+    $LOGIN = 'root';
+    $MDP = 'iutinfo';
+
+    // tentative de connexion à la BD
+    try {
+        // connexion à la BD
+        $linkpdo = new PDO("mysql:host=$SERVER;dbname=$DB", $LOGIN, $MDP);
+    } catch (Exception $e) {
+        die('Erreur ! Problème de connection à la base de données : ' . $e->getMessage());
     }
 
-	
-	$users = array(
-		array(
-			"username" => "CbrtLine",
-			"password" => "iutinfo",
-			"role" => "Publisher"
-		),
-		array(
-			"username" => "AlrdFabien",
-			"password" => "FabInfo",
-			"role" => "Moderator"
-		)
-	);
+    // retourne la connection
+    return $linkpdo;
+}
 
-	function isValidUser($username, $password){
+function get($id){
 
-		global $users;
-
-		foreach ($users as $user) {
-		    if ($user["username"] == $username) {
-		    	if ($user["password"] == $password){
-		    		return True;
-		    	}
-		    	return False;
-		    }
-		}
-		return False;
-	}
-
-	/// Permet de récuperer le rôle d'un utilisateur
-	/// Cette fonction n'est appelée qu'une fois que l'identification est vérifiée
-	function get_user_role($username) {
-
-		global $users;
-
-		foreach ($users as $user) {
-		    if ($user["username"] == $username) {
-		    	return $user["role"];
-		    }
-		}
-
-		// Si aucun utilisateur n'est trouvé avec le nom d'utilisateur spécifié, retourner null
-		return null;
-	}
-
-
-    function getId($linkpdo, $id)
-    {
-        if(empty($id) || !is_numeric($id)) {
-            return null;
-        }
-        // preparation de la Requête sql
-        try{
-            $req = $linkpdo->prepare('select * from articles where id = :id');
-        } catch(PDOException $e) {
-            return null;
-        }
-        
-        if($req->bindParam(':id', $id, PDO::PARAM_INT)) {
-            if($req->execute()) {
-                return $req->fetchAll(PDO::FETCH_ASSOC);
-            }
-        }
-        return null;
+    // Vérification de l'id
+    if (!is_numeric($id)){
+        return SYNTAXE;
     }
 
+    $linkpdo = connexionBd();
 
+    // preparation de la Requête sql
+    $req = $linkpdo->prepare('select * from articles where id = :id');
+    if ($req == false) {
+        die('Erreur !');
+    }
+    // execution de la Requête sql
+    $req->execute(array('id' => $id));
+    if ($req == false) {
+        die('Erreur !');
+    }
+    return $req->fetchAll();
+}
 
-    function getAll($linkpdo)
-    {
-        try{
-            $req = $linkpdo->prepare('select * from articles');
-        }catch(PDOException $e){
-            return null;
-        }
-        
-        if ($req == false) {
-            die('Erreur ! GetAll');
-        }
-        if($req->execute()) {
-            return $req->fetchAll(PDO::FETCH_ASSOC);
+function getAll()
+{
+    $linkpdo = connexionBd();
+    // preparation de la Requête sql
+    $req = $linkpdo->prepare('select * from articles');
+    if ($req == false) {
+        die('Erreur ! GetAll');
+    }
+    // execution de la Requête sql
+    $req->execute();
+    if ($req == false) {
+        die('Erreur ! GetAll');
+    }
+    return $req->fetchAll();
+}
+
+function post($publication, $auteur)
+{
+    $linkpdo = connexionBd();
+
+    // preparation de la Requête sql
+    $req = $linkpdo->prepare('
+        insert into articles 
+        (publication,auteur,like_pub,dislike,date, signalement) 
+        value(:publication,:auteur, 0,0,NOW(),0)
+        ');
+
+    if ($req == false) {
+        die('Erreur ! Post');
+    }
+
+    //Définition des paramètres
+    $param = [
+        [':publication', $publication, PDO::PARAM_STR],
+        [':auteur', $auteur, PDO::PARAM_STR],
+    ];
+
+    //teste des paramètre
+    foreach ($param as $params) {
+        if (!$req->bindParam($params[0], $params[1], $params[2])) {
+            return ERREUR_SQL;
         }
     }
 
-    function post($publication, $linkpdo, $jwt) {
-        try{
-            $req = $linkpdo->prepare('insert into articles (publication,auteur,like_pub,dislike,date, signalement) value(:publication,:auteur, 0,0,NOW(),0)');
-        }catch(PDOException $e) {
-            return null;
-        }
-        if ($req -> bindParam(':publication', $publication, PDO::PARAM_STR, ':auteur',get_role_jwt($jwt), PDO::PARAM_STR)) {
-            if($req->execute()) {
-                return $req->fetchAll(PDO::FETCH_ASSOC);
-            }
-        $id = $linkpdo->lastInsertId();
-        return getId($id);
-    }
-        }
+    if (!$req->execute()){
+        $linkpdo->rollback();
+        return ERREUR_SQL;
+    } 
 
+    // recuperation du dernier id
+    $lastId = $linkpdo->lastInsertId();
+    $linkpdo->commit();
+    return getId($lastId);
+}
 
-    function put($id, $publication, $linkpdo)
-    {
-        if(empty($id) || !is_numeric($id)) {
-            return null;
-        }
+function put($id, $publication)
+{
+    $linkpdo = connexionBd();
 
-        try {
-            // preparation de la Requête sql
-            $req = $linkpdo->prepare('update articles set publication = :publication where id = :id');
-        } catch(PDOException $e) {
-            return null;
-        }
-        if ($req -> bindParam(':publication', $publication, PDO::PARAM_STR, ':id', $id, PDO::PARAM_INT)) {
-            if($req->execute()) {
-                return $req->fetchAll(PDO::FETCH_ASSOC);
-            }
+    // preparation de la Requête sql
+    $req = $linkpdo->prepare('
+        update articles 
+        set publication = :publication 
+        where id = :id
+        ');
 
-        }
+    if ($req == false) {
+        die('Erreur ! Put');
     }
 
-    function delete($id, $linkpdo){
-        if(empty($id) || !is_numeric($id)) {
-            return null;
-        }
-        try {
-            $req = $linkpdo->prepare('delete from articles where id = :id');
-        } catch(PDOException $e) {
-            return null;
-        }
-        if($req -> bindParam(':id', $id, PDO::PARAM_INT)) {
-            if($req->execute()){
-                return $req->fetchAll(PDO::FETCH_ASSOC);
-            }
+    //Définition des paramètres
+    $param = [
+        [':id', $id, PDO::PARAM_INT],
+        [':publication', $publication, PDO::PARAM_STR],
+    ];
+
+    //teste des paramètre
+    foreach ($param as $params) {
+        if (!$req->bindParam($params[0], $params[1], $params[2])) {
+            return ERREUR_SQL;
         }
     }
 
-    function get_role_jwt($jwt) {
+    if (!$req->execute()){
+        $linkpdo->rollback();
+        return ERREUR_SQL;
+    } 
 
-        // Vérification de validité
-        if (!is_jwt_valid($jwt)) {
-            // Jeton invalide ou expiré
-            return null;
-        }
+    // recuperation du dernier id
+    $lastId = $linkpdo->lastInsertId();
+    $linkpdo->commit();
+    return getId($lastId);
+}
 
-        $payload = base64_decode(explode('.', $jwt)[1]);
-        $decoded_payload = json_decode($payload, true);
+function delete($id)
+{
+    $linkpdo = connexionBd();
 
-        if (!isset($decoded_payload['role'])) {
-            // Rôle non trouvé dans le jeton
-            return null;
-        }
+    // preparation de la Requête sql
+    $req = $linkpdo->prepare('
+        delete 
+        from articles 
+        where id = :id
+        ');
 
-        return $decoded_payload['role'];
+    if ($req == false) {
+        die('Erreur ! Delete');
     }
 
-    function get_usename_jwt($jwt) {
-
-        // Vérification de validité
-        if (!is_jwt_valid($jwt)) {
-            // Jeton invalide ou expiré
-            return null;
-        }
-
-        $payload = base64_decode(explode('.', $jwt)[1]);
-        $decoded_payload = json_decode($payload, true);
-
-        if (!isset($decoded_payload['username'])) {
-            // Rôle non trouvé dans le jeton
-            return null;
-        }
-
-        return $decoded_payload['username'];
+    if (!is_numeric($id)){
+        deliver_response(400, "Erreur de syntaxe, ID invalide", NULL);
     }
 
-    function is_id_exist($id) {
-        // Connexion à la BD
-        $linkpdo = connexionBd();
-        
-        // Requête pour vérifier si l'id existe dans la table appropriée
-        $req = $linkpdo->prepare("SELECT id FROM articles WHERE id=:id");
-        $req->bindParam(':id', $id);
-        $req->execute();
-        
-        // Si l'id est trouvé, renvoie true, sinon false
-        if ($req->rowCount() > 0) {
-            return true;
-        } else {
-            return false;
-        }
+    if (!$req->execute(array('id' => $id))){
+        $linkpdo->rollback();
+        return ERREUR_SQL;
     }
-?>
+
+    deliver_response(200, "Article supprimé", NULL);
+
+}
--- APIserveur.php
+++ APIserveur.php
@@ -1,127 +1,197 @@
 <?php
-	include('jwt_utils.php');	
-	/// Librairies éventuelles (pour la connexion à la BDD, etc.)
+	include('jwt_utils.php');
 	include('APImylib.php');
-	include('Erreurs.php');
+	include('Fonctions.php');
 
 	/// Paramétrage de l'entête HTTP (pour la réponse au Client)
 	header("Content-Type:application/json");
 
-	$bearer_token = '';
-	$bearer_token = get_bearer_token();
-
 	$linkedPDO = connexionBd();
 
 	/// Identification du type de méthode HTTP envoyée par le client
-	$http_method = $_SERVER['REQUEST_METHOD'];	
-	if(is_jwt_valid($bearer_token)){
-		switch ($http_method) {
-		
+	$http_method = $_SERVER['REQUEST_METHOD'];
 
-		
 
-		    /// Cas de la méthode GET
-		    case "GET":
-		        try {
-		            //! Traitement
-		            if (isset($_GET['id'])) {
-						$matchingData = getId($linkedPDO, $_GET['id']);
-						if(is_null($matchingData)) {
-							deliver_response(500, "Erreur", null);
-						}
-						deliver_response(200, "Voici les articles", $matchingData);
-		            } else {
-		                $matchingData = getALL();
-		            }
-		            $RETURN_CODE = 200;
-		            $STATUS_MESSAGE = "modification réussi";
-		        } catch (\Throwable $th) {
-		            $RETURN_CODE = $th->getCode();
-		            $STATUS_MESSAGE = $th->getMessage();
-		        } finally {
-		            //! Envoi de la réponse au Client
-		            deliver_response($RETURN_CODE, $STATUS_MESSAGE, $matchingData);
-		        }
+	switch ($http_method) {
+
+	    /// Cas de la méthode GET
+	    case "GET":
+
+		    $bearer_token = '';
+			$bearer_token = get_bearer_token();	
+
+			echo $bearer_token;
+
+			// Vérification de la validation du jeton
+			if(!is_jwt_valid($bearer_token)){
+				deliver_response(500, "erreur de validité de jeton", NULL);
+				break;
+			//Si jeton valide, on récupère le rôle de l'utilisateur
+			}else{
+				$role = get_role_jwt($bearer_token);
+			}
+			
+            if (isset($_GET['id'])) {
+
+                // Vérification syntaxe de l'id
+                if (!is_numeric($_GET['id'])) {
+                	//erreur de syntaxe ID invalide
+                	deliver_response(400, "Erreur de syntaxe, ID invalide", NULL);
+                	break;
+                } 
+
+                // Vérifie que l'id existe
+                if (!is_id_exist($_Get['id'])) {
+                	//L'identifiant n'existe pas 
+                	deliver_response(404, "ID inconnu", NULL);
+                	break;
+                }
+
+                //Pour savoir quel Get on traîte, on regarde le contenu du body/url
+
+                $matchingData = get($_GET['id']);
+
+            } else {
+                $matchingData = getALL();
+            }
+
+            get_erreur($matchingdata, "Article trouvé", "Article : ".$matchingData);
+
+	        break;
+	        
+
+	    /// Cas de la méthode POST
+	    case "POST":
+
+	    	$bearer_token = '';
+			$bearer_token = get_bearer_token();	
+
+			// Vérification de la validation du jeton
+			if(!is_jwt_valid($bearer_token)){
+				deliver_response(500, "erreur de validité de jeton", NULL);
+				break;
+
+			//Si jeton valide, on récupère le rôle de l'utilisateur
+			}else{
+				$role = get_role_jwt($bearer_token);
+			}
+
+	    	if ($role == 'Publisher'){
+
+	    		/// Récupération des données envoyées par le Client
+	            $postedData = file_get_contents('php://input');
+	            $postedData = json_decode($postedData, true);
+
+	            /// Traitement
+	            $matchingData = post($postedData['phrase']);
+
+		    	get_erreur($matchingdata, "Article ajouté", "Article : ".$matchingData);
+
 		        break;
+		    }
 
+	    /// Cas de la méthode PUT
+	    case "PUT":
 
-		    /// Cas de la méthode POST
-		    case "POST":
-		        try {
+	    	$bearer_token = '';
+			$bearer_token = get_bearer_token();	
+
+			// Vérification de la validation du jeton
+			if(!is_jwt_valid($bearer_token)){
+				deliver_response(500, "erreur de validité de jeton", NULL);
+				break;
+
+			//Si jeton valide, on récupère le rôle de l'utilisateur
+			}else{
+				$role = get_role_jwt($bearer_token);
+			}
+
+	    	if ($role == 'Publisher'){
+
+	    		if (isset($_GET['id'])) {
+
+	                // Vérification syntaxe de l'id
+	                if (!is_numeric($_GET['id'])) {
+	                	//erreur de syntaxe ID invalide
+	                	deliver_response(400, "Erreur de syntaxe, ID invalide", NULL);
+	                	break;
+	                } 
+
+	                // Vérifie que l'id existe
+	                if (!is_id_exist($_Get['id'])) {
+	                	//L'identifiant n'existe pas 
+	                	deliver_response(404, "ID inconnu", NULL);
+	                	break;
+	                }
+
 		            /// Récupération des données envoyées par le Client
 		            $postedData = file_get_contents('php://input');
 		            $postedData = json_decode($postedData, true);
+
 		            /// Traitement
-		            $matchingData = post($postedData['phrase']);
-		            $RETURN_CODE = 200;
-		            $STATUS_MESSAGE = "modification réussi";
-		        } catch (\Throwable $th) {
-		            $RETURN_CODE = $th->getCode();
-		            $STATUS_MESSAGE = $th->getMessage();
-		        } finally {
-		            //! Envoi de la réponse au Client
-		            deliver_response($RETURN_CODE, $STATUS_MESSAGE, $matchingData);
+		            $matchingData = put($_GET['id'], $postedData['phrase']);
+
+			    	get_erreur($matchingdata, "Article modifié", "Article : ".$matchingData);
+	            
+	            }else{
+                	//ID non renseigné
+                	deliver_response(400, "Erreur, identifiant manquant", NULL);
+                	break;
+	            }
+
+		        break;
+		    }
+
+	    /// Cas de la méthode DELETE
+	    case "DELETE":
+
+	    	$bearer_token = '';
+			$bearer_token = get_bearer_token();	
+
+			// Vérification de la validation du jeton
+			if(!is_jwt_valid($bearer_token)){
+				deliver_response(500, "erreur de validité de jeton", NULL);
+				break;
+
+			//Si jeton valide, on récupère le rôle de l'utilisateur
+			}else{
+				$role = get_role_jwt($bearer_token);
+			}
+
+    		if (isset($_GET['id'])) {
+
+                // Vérification syntaxe de l'id
+                if (!is_numeric($_GET['id'])) {
+                	//erreur de syntaxe ID invalide
+                	deliver_response(400, "Erreur de syntaxe, ID invalide", NULL);
+                	break;
+                } 
+
+                // Vérifie que l'id existe
+                if (!is_id_exist($_Get['id'])) {
+                	//L'identifiant n'existe pas 
+                	deliver_response(404, "ID inconnu", NULL);
+                	break;
+                }
+
+                //On traite les différent cas par rôle
+                if ($role == 'Publisher'){
+	                /// Traitement pour un publisher
+	                $matchingData = deleteByPublisher($_GET['id']);
+	            }else {
+	                /// Traitement pour tout autre rôle
+	                $matchingData = delete($_GET['id']);
 		        }
-		        break;
+            }
 
-		    /// Cas de la méthode PUT
-		    case "PUT":
-		        try {
-		            /// Exception
-		            if (empty($_GET['id'])) {
-		                throw new Exception("Missing id", 400);
-		            }
-		            /// Récupération des données envoyées par le Client
-		            $postedData = file_get_contents('php://input');
-		            $postedData = json_decode($postedData, true);
-		            /// Traitement
-		            if (isset($_GET['id'])) {
-		                $matchingData = put($_GET['id'], $postedData['phrase']);
-		            }
-		            $RETURN_CODE = 200;
-		            $STATUS_MESSAGE = "modification réussi";
-		        } catch (\Throwable $th) {
-		            $RETURN_CODE = $th->getCode();
-		            $STATUS_MESSAGE = $th->getMessage();
-		        } finally {
-		            /// Envoi de la réponse au Client
-		            deliver_response($RETURN_CODE, $STATUS_MESSAGE, $matchingData);
-		        }
-		        break;
+			get_erreur($matchingdata, "Article supprimé", NULL);
 
-		    /// Cas de la méthode DELETE
-		    case "DELETE":
-		        try {
-		            /// Exception
-		            if (empty($_GET['id'])) {
-		                throw new Exception("Missing id", 400);
-		            }
-		            /// Récupération de l'identifiant de la ressource envoyé par le Client
-		            if (isset($_GET['id'])) {
-		                /// Traitement
-		                $matchingData = delete($_GET['id']);
-		            }
-		            $RETURN_CODE = 200;
-		            $STATUS_MESSAGE = "modification réussi";
-		        } catch (\Throwable $th) {
-		            $RETURN_CODE = $th->getCode();
-		            $STATUS_MESSAGE = $th->getMessage();
-		        } finally {
-		            //! Envoi de la réponse au Client
-		            deliver_response($RETURN_CODE, $STATUS_MESSAGE, $matchingData);
-		        }
-		        break;
+	        break;
 
-		    default:
-		        $matchingData = null;
-		        $RETURN_CODE = 404;
-		        $STATUS_MESSAGE = "Méthode introuvable";
-		        deliver_response($RETURN_CODE, $STATUS_MESSAGE, $matchingData);
-		        break;
-		}
-		
-		
+	    default:
 
+	        deliver_response(404, "Méthode introuvable", null);
+	        break;
 	}
 	
 ?>
